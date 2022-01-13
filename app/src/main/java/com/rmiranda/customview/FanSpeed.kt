bc618262942afb0593d8f24f17b49153fd077ba1
Rodrigo Miranda <rmiranda@applaudostudios.com>
2022-01-13 11:16:10 -0600

    ✨ Add FanSpeed enum class

---
+++ app/src/main/java/com/rmiranda/customview/FanSpeed.kt
@@ -0,0 +1,11 @@
+package com.rmiranda.customview
+
+/**
+ * Created by rodrigomiranda on 13/1/22.
+ */
+internal enum class FanSpeed(val label: Int) {
+    OFF(R.string.fan_off),
+    LOW(R.string.fan_low),
+    MEDIUM(R.string.fan_medium),
+    HIGH(R.string.fan_high)
+}
